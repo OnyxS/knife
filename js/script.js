87563871e3bea15f292345d327831eeea3f22ed8
OnyxS <64073279+OnyxS@users.noreply.github.com>
2021-06-27 02:59:03 +0300

    debug cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+var menuOpen=document.querySelectorAll(".header__ham");
+var contentLeft=document.querySelector(".content__left");
+
+menuOpen.forEach(function(button){
+	button.addEventListener("click", toggleMenu);
+	});
+
+function toggleMenu() {
+
+	  contentLeft.classList.toggle("showMenu");
+	
+ }
+
+
+ var itemName=document.querySelectorAll(".item__name");
+ var max=0;
+  itemName.forEach(function(i){
+	 var h=i.clientHeight;
+	 if(max<h) max=h;
+  });
+ 
+  itemName.forEach(function(item){
+	 item.style.height=max+'px';
+  });
+
+
+
 const start=new Swiper('.start__swiper',{
 	
 	pagination:{
@@ -32,16 +59,5 @@ const recom=new Swiper('.recomendation__swiper',{
 	slidesPerGroup:4,
 });
 
-var menuOpen=document.querySelectorAll(".header__ham");
-var contentLeft=document.querySelector(".content__left");
 
-menuOpen.forEach(function(button){
-	button.addEventListener("click", toggleMenu);
-	});
-
-function toggleMenu() {
-
-	  contentLeft.classList.toggle("showMenu");
-	
- }
 
